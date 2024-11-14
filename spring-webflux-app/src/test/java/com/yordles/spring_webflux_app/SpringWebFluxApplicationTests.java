2a5b29743c05c871980cbbc0496cc1d5b102d1ed
Stiven Ortiz Noreña <stivenestortiz@gmail.com>
2024-11-13 21:23:15 -0500

    feat: initial commit

---
+++ spring-webflux-app/src/test/java/com/yordles/spring_webflux_app/SpringWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yordles.spring_webflux_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
